cb2f358b1ad48fb0e3020af4acddf0713b00192f
MaryHooker <mc020791@yahoo.com>
2020-03-06 15:35:26 -0600

    some js

--- reflections.js
+++ reflections.js
@@ -18,29 +18,55 @@ let confidence = document.querySelector('#confidence');
 let submitButton = document.querySelector('.save');
 //sanity
 // console.log(submitButton);
+let displayInBrowser = document.querySelector('.renderedReflection')
+//sanity
+// console.log(displayInBrowser);
+
+let reflectionEntries = [];
+
+//Create a function that stores the users input and pushes to array
+let onClick = (event) => {
+    event.preventDefault();
+    //Create a journal entries array with object literal notation
+    reflectionEntries = [];
+    //Create object literal notation to store user input
+    let everyEntry = {
+        creation_date: reflectionDate,
+        journal_entry: reflectionText,
+        confidence_level: confidence
+    };
+    reflectionEntries.push(everyEntry);
+
+    // reflectionEntries.forEach();
+    //display the inputs inside the browser
+    displayInBrowser.innerText= `Reflection date: ${everyEntry.creation_date.value}\nReflection: ${everyEntry.journal_entry.value}.\nConfidence level: ${everyEntry.confidence_level.value}`;
+
+    console.log(reflectionEntries);
+}
 
 
-//create a function that stores the users input
-let onClick = (event) =>{
-    console.log(
-        `The reflection date is: ${reflectionEntries.creation_date}.
-        Their reflection is: ${reflectionEntries.journal_entry}.Their confidence level is: ${reflectionEntries.confidence_level}.`
-    )
-};
 //create an event listener for the submit button
 submitButton.addEventListener('click', onClick);
 
 
 
-//Create a journal entries array with object literal notation
 
-const reflectionEntries = [
-    {
-        creation_date: reflectionDate.value,
-        journal_entry: reflectionText.value, 
-        confidence_level: confidence.value
-    }
+
+
+
+
+
+
+// renderReflectionJournalEntries();
+
+// let renderReflectionJournalEntries = () => {
     
-];
+// };
 
-reflectionEntries.forEach(onClick)
+//   `The reflection date is: ${reflectionEntries.creation_date}.
+//   Their reflection is: ${reflectionEntries.journal_entry}.Their confidence level is: ${reflectionEntries.confidence_level}.`
+// );
+
+// everyEntry.creation_date = reflectionDate.value;
+//     everyEntry.journal_entry = reflectionText.value;
+//     everyEntry.confidence_level = confidence.value;

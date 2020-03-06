4f1a37451d98b930600e4fb66c2e92d1fbbeb918
MaryHooker <mc020791@yahoo.com>
2020-03-06 12:01:32 -0600

    js progress

--- reflections.js
+++ reflections.js
@@ -2,33 +2,23 @@
 //store reflection date in variable
 let reflectionDate = document.querySelector('#date');
 //sanity
-console.log(reflectionDate);
+// console.log(reflectionDate);
 //store reflection text in variable
 let reflectionText = document.querySelector('#reflectionText');
 //sanity
-console.log(reflectionText);
+// console.log(reflectionText);
 //store
 
 //get reference to radio buttons
 let confidence = document.querySelector('#confidence');
 //sanity
-console.log(confidence);
+// console.log(confidence);
 
 //get reference to submit reflections button
 let submitButton = document.querySelector('.save');
 //sanity
-console.log(submitButton);
+// console.log(submitButton);
 
-//Create a journal entries array with object literal notation
-
-const reflectionEntries = [
-    {
-        creation_date: reflectionDate.value,
-        journal_entry: reflectionText.value, 
-        confidence_level: confidence.value
-    }
-    
-];
 
 //create a function that stores the users input
 let onClick = (event) =>{
@@ -42,4 +32,15 @@ submitButton.addEventListener('click', onClick);
 
 
 
+//Create a journal entries array with object literal notation
 
+const reflectionEntries = [
+    {
+        creation_date: reflectionDate.value,
+        journal_entry: reflectionText.value, 
+        confidence_level: confidence.value
+    }
+    
+];
+
+reflectionEntries.forEach(onClick)

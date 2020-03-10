c7519985f8d7a61fe2c27c7bece9c1707887bc14
MaryHooker <mc020791@yahoo.com>
2020-03-09 21:38:07 -0500

    reformatted html & css

--- reflections.js
+++ reflections.js
@@ -18,35 +18,50 @@ let confidence = document.querySelector('#confidence');
 let submitButton = document.querySelector('.save');
 //sanity
 // console.log(submitButton);
-let displayInBrowser = document.querySelector('.renderedReflection')
+//Created for my empty space in browser for the users reflections
+let displayInBrowser = document.querySelector('#recent')
 //sanity
 // console.log(displayInBrowser);
 
+//Create an empty journal entries array
+let reflectionEntries = [];
 
 //Create a function that stores the users input and pushes to array
 let onClick = (event) => {
     event.preventDefault();
-    //Create a journal entries array with object literal notation
-    let reflectionEntries = [];
+    
     //Create object literal notation to store user input
     let everyEntry = {
         creation_date: reflectionDate.value,
         journal_entry: reflectionText.value,
         confidence_level: confidence.value
     };
+    //push the objects into the empty reflections array
     reflectionEntries.push(everyEntry);
+    //call function that iterates through the array and display each object in the browser
+    displayEntries(reflectionEntries);
 
-    
-    //display the inputs inside the browser
-    displayInBrowser.innerText= `Date: ${reflectionEntries[0].creation_date}\nReflection: ${reflectionEntries[0].journal_entry}\nConfidence Level: ${reflectionEntries[0].confidence_level}`;
+};
 
-    // console.log(reflectionEntries);
-    // reflectionEntries.forEach();
+
+//Create a function that uses the empty string as its parameters and replaces the innerText in the browser
+let displayEntries = (reflectionEntries) => {
+    //create empty string
+    let reflectionString='';
+    //create function that iterates through every object in the array
+    reflectionEntries.forEach(element =>
+    {   //build template literal that will list all of the users data
+        reflectionString=`${reflectionString} Date: ${element.creation_date}\nEntry: ${element.journal_entry}\nConfidence: ${element.confidence_level}\n\n`;
+        
+    });
+    //display user inputs inside empty p tags of the 
+    displayInBrowser.innerText= `${reflectionString}`;
 }
-
-
 //create an event listener for the submit button
 submitButton.addEventListener('click', onClick);
 
 
+
+
+   
 
